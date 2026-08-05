73604fb756d558a7b7edfeb620f2126e937b41e2
Aman Faizan <174135263+Amanfaizz@users.noreply.github.com>
2026-08-05 19:40:03 +0530

    add rock-paper-scissor project

---
+++ Rock-paper-scissor/script.js
@@ -0,0 +1,95 @@
+        const score ={
+            wins : 0,
+            loss :0,
+            tie : 0 
+        }
+        
+        
+        
+        function computersMove() {
+            const randNumber = Math.random();
+            let computerMove = '';
+            if (randNumber >= 0 && randNumber < 1 / 3) {
+                computerMove = 'Rock';
+            }
+            else if (randNumber >= 1 / 3 && randNumber < 2 / 3) {
+                computerMove = 'Paper';
+            }
+            else {
+                computerMove = 'Scissor';
+            }
+            return computerMove;
+        }
+
+        function playGame(playerMove) {
+            let result = ''
+            const computerMove = computersMove();
+            if (playerMove === 'Rock') {
+                if (computerMove === 'Rock') {
+                    result = 'tie'
+                }
+                else if (computerMove === 'Paper') {
+                    result = 'You Lose'
+                }
+                else {
+                    result = 'You Win';
+                }
+
+           
+                
+
+            }
+
+            else if (playerMove === 'Paper') {
+                if (computerMove === 'Paper') {
+                    result = 'tie'
+                }
+                else if (computerMove === 'Scissor') {
+                    result = 'You Lose'
+                }
+                else {
+                    result = 'You Win'
+                }
+            }
+
+            else {
+                if (computerMove === 'Scissor') {
+                    result = 'tie'
+                }
+                else if (computerMove === 'Rock') {
+                    result = 'You Lose'
+                }
+                else {
+                    result = 'You Win'
+                }
+            }
+             if(result==='You Win'){
+                score.wins ++;
+            }
+
+            else if(result==='You Lose'){
+                score.loss ++;
+            }
+
+            else{
+                score.tie ++;
+            }
+
+           
+                alert(`you chose: ${playerMove}. Computer chose ${computerMove} , result ${result}`)
+
+
+                
+        console.log(score);
+
+        }
+
+
+        function reset(){
+           score.wins = 0;
+           score.loss = 0;
+           score.tie = 0;
+
+           console.log(score);
+
+        }
